314ca7011660a34c55de7f8b3df4021ce6814da9
lettemax <maxlettenberger@gmail.com>
2021-03-24 14:14:30 -0500

    add projects

---
+++ index.js
@@ -0,0 +1,36 @@
+// predefine projects with details
+let projects = {
+    "aurora": {
+        "tech-used": [
+            "JS", "HTML", "CSS", "Express", "PostgreSQL", "Pug"
+        ],
+        "description": "Clone of Quora where users can sort questions by expertise level",
+        "live-link": "https://aurora-quora.herokuapp.com/",
+        "github-repo": "https://github.com/cellomatt/aurora"
+    },
+    "robinkarma": {
+        "tech-used": [
+            "React", "Redux", "JS", "HTML", "CSS", "Express", "PostgreSQL"
+        ],
+        "description": "Clone of Robinhood where users buy shares of charitable projects",
+        "live-link": "https://robinhood-karma.herokuapp.com/",
+        "github-repo": "https://github.com/lettemax/authenticate-me"
+    },
+    "petstagram": {
+        "tech-used": [
+            "React", "Redux", "JS", "Python", "HTML", "CSS", "Express", "PostgreSQL", "Flask", "SQLAlchemy", "AWS S3"
+        ],
+        "description": "Clone of Instagram where users post photos of their pets",
+        "live-link": "https://petstagram-3.herokuapp.com/",
+        "github-repo": "https://github.com/lettemax/petstagram"
+    },
+    "zipnodes": {
+        "tech-used": [
+            "React", "Redux", "JS", "Python", "HTML", "CSS", "Express", "PostgreSQL", "Flask", "SQLAlchemy"
+        ],
+        "description": "Site for aspiring utilitarians to donate, volunteer and participate in politics",
+        "live-link": "https://zipnodes.herokuapp.com/",
+        "github-repo": "https://github.com/lettemax/zipnodes"
+    },
+    
+}
