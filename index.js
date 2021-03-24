9fc419bdcd255d9b34199a8466d7de82a69e48d8
lettemax <maxlettenberger@gmail.com>
2021-03-24 14:46:56 -0500

    add js for page loading / project toggling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // predefine projects with details
-let projects = {
+const projects = {
     "aurora": {
+        "name": "aurora",
         "tech-used": [
             "JS", "HTML", "CSS", "Express", "PostgreSQL", "Pug"
         ],
@@ -9,6 +10,7 @@ let projects = {
         "github-repo": "https://github.com/cellomatt/aurora"
     },
     "robinkarma": {
+        "name": "robinkarma",
         "tech-used": [
             "React", "Redux", "JS", "HTML", "CSS", "Express", "PostgreSQL"
         ],
@@ -17,6 +19,7 @@ let projects = {
         "github-repo": "https://github.com/lettemax/authenticate-me"
     },
     "petstagram": {
+        "name": "petstagram",
         "tech-used": [
             "React", "Redux", "JS", "Python", "HTML", "CSS", "Express", "PostgreSQL", "Flask", "SQLAlchemy", "AWS S3"
         ],
@@ -25,12 +28,46 @@ let projects = {
         "github-repo": "https://github.com/lettemax/petstagram"
     },
     "zipnodes": {
+        "name": "zipnodes",
         "tech-used": [
             "React", "Redux", "JS", "Python", "HTML", "CSS", "Express", "PostgreSQL", "Flask", "SQLAlchemy"
         ],
         "description": "Site for aspiring utilitarians to donate, volunteer and participate in politics",
         "live-link": "https://zipnodes.herokuapp.com/",
         "github-repo": "https://github.com/lettemax/zipnodes"
-    },
-    
+    }
+};
+
+// when page loads add project titles to projects-bar, default details to first project
+window.addEventListener('DOMContentLoaded', event => {
+    // get projects-bar
+    const projBar = document.getElementById("projects-bar");
+    // get project names
+    let projs = Object.keys(projects);
+    // create a project bar item for each project
+    projs.forEach(proj => {
+        let projBarItem = document.createElement("p");
+        projBarItem.innerHTML = proj;
+        projBarItem.classList.add("project");
+        // add event listener to toggle project details on click
+        projBarItem.addEventListener("click", event => {
+            updateProjDetails(proj);
+        });
+        projBar.appendChild(projBarItem);
+    });
+    // load first project details
+    updateProjDetails(projs[0]);
+});
+
+// function to update project details
+function updateProjDetails(proj) {
+    let techUsed = document.getElementById("tech-used");
+    techUsed.innerHTML = projects[proj]["tech-used"];
+    let description = document.getElementById("description");
+    description.innerHTML = projects[proj]["description"];
+    let liveLink = document.getElementById("live-link");
+    liveLink.innerHTML = projects[proj]["live-link"];
+    let githubRepo = document.getElementById("github-repo");
+    githubRepo.innerHTML = projects[proj]["github-repo"];
 }
+
