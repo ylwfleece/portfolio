c11ef6ebfb448525b4ed0f9c29f5a464bbd3df18
lettemax <maxlettenberger@gmail.com>
2021-03-24 15:35:18 -0500

    convert tech used to string vs arr

--- index.js
+++ index.js
@@ -2,36 +2,28 @@
 const projects = {
     "aurora": {
         "name": "aurora",
-        "tech-used": [
-            "JS", "HTML", "CSS", "Express", "PostgreSQL", "Pug"
-        ],
+        "tech-used": "JS, HTML, CSS, Express, PostgreSQL, Pug",
         "description": "Clone of Quora where users can sort questions by expertise level",
         "live-link": "https://aurora-quora.herokuapp.com/",
         "github-repo": "https://github.com/cellomatt/aurora"
     },
     "robinkarma": {
         "name": "robinkarma",
-        "tech-used": [
-            "React", "Redux", "JS", "HTML", "CSS", "Express", "PostgreSQL"
-        ],
+        "tech-used": "React, Redux, JS, HTML, CSS, Express, PostgreSQL",
         "description": "Clone of Robinhood where users buy shares of charitable projects",
         "live-link": "https://robinhood-karma.herokuapp.com/",
         "github-repo": "https://github.com/lettemax/authenticate-me"
     },
     "petstagram": {
         "name": "petstagram",
-        "tech-used": [
-            "React", "Redux", "JS", "Python", "HTML", "CSS", "Express", "PostgreSQL", "Flask", "SQLAlchemy", "AWS S3"
-        ],
+        "tech-used": "React, Redux, JS, Python, HTML, CSS, Express, PostgreSQL, Flask, SQLAlchemy, AWS S3",
         "description": "Clone of Instagram where users post photos of their pets",
         "live-link": "https://petstagram-3.herokuapp.com/",
         "github-repo": "https://github.com/lettemax/petstagram"
     },
     "zipnodes": {
         "name": "zipnodes",
-        "tech-used": [
-            "React", "Redux", "JS", "Python", "HTML", "CSS", "Express", "PostgreSQL", "Flask", "SQLAlchemy"
-        ],
+        "tech-used": "React, Redux, JS, Python, HTML, CSS, Express, PostgreSQL, Flask, SQLAlchemy",
         "description": "Site for aspiring utilitarians to donate, volunteer and participate in politics",
         "live-link": "https://zipnodes.herokuapp.com/",
         "github-repo": "https://github.com/lettemax/zipnodes"
